6c36f61f521588f2db5ceb929a613ef3524e7171
Sojty <sojak.vit@gmail.com>
2026-05-02 18:43:49 +0200

    feat(API)!: attempt more idiomatic API
    from u8 instead new in PlayerId
    
    add PlayerIdError {Zero}
    
    add impl fns for Player

--- crates/domain/src/player.rs
+++ crates/domain/src/player.rs
@@ -1,25 +1,80 @@
 use serde::{Deserialize, Serialize};
-use std::num::NonZeroU8;
+use std::{fmt, num::NonZeroU8};
 
-#[derive(Clone, Copy, Serialize, Deserialize)]
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(transparent)]
 pub struct PlayerId(NonZeroU8);
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum PlayerIdError {
+    Zero,
+}
+
+impl fmt::Display for PlayerIdError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PlayerIdError::Zero => f.write_str("player id must be non-zero"),
+        }
+    }
+}
+
+impl std::error::Error for PlayerIdError {}
+
 impl PlayerId {
-    pub fn new(id: u8) -> Self {
-        Self(NonZeroU8::new(id).unwrap())
-    }
-
-    pub fn try_new(id: u8) -> Option<Self> {
-        NonZeroU8::new(id).map(Self)
-    }
-
+    #[must_use]
     pub fn get(self) -> u8 {
         self.0.get()
     }
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+impl From<PlayerId> for u8 {
+    fn from(id: PlayerId) -> Self {
+        id.get()
+    }
+}
+
+// impl From<PlayerId> for NonZeroU8 {
+//     fn from(id: PlayerId) -> Self {
+//         id.0
+//     }
+// }
+
+impl TryFrom<u8> for PlayerId {
+    type Error = PlayerIdError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        NonZeroU8::new(value)
+            .map(PlayerId)
+            .ok_or(PlayerIdError::Zero)
+    }
+}
+
+// impl From<NonZeroU8> for PlayerId {
+//     fn from(value: NonZeroU8) -> Self {
+//         Self(value)
+//     }
+// }
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Player {
-    pub id: PlayerId,
-    pub nick: String,
+    id: PlayerId,
+    nick: String,
+}
+
+impl Player {
+    pub fn new(id: PlayerId, nick: impl Into<String>) -> Self {
+        Self {
+            id,
+            nick: nick.into(),
+        }
+    }
+
+    pub fn id(&self) -> PlayerId {
+        self.id
+    }
+
+    pub fn nick(&self) -> &str {
+        &self.nick
+    }
 }
